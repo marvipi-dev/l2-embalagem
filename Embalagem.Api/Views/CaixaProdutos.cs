200b1d231c0dc5259141cf37612a5550545ae81d
Marcus Vinicius Pires <211013457+marvipi-dev@users.noreply.github.com>
2025-06-01 16:15:03 -0300

    Don't serialize null observations

--- Embalagem.Api/Views/CaixaProdutos.cs
+++ Embalagem.Api/Views/CaixaProdutos.cs
@@ -7,5 +7,7 @@ public class CaixaProdutos
     [JsonPropertyName("caixa_id")] public string? CaixaId { get; set; }
 
     public IEnumerable<string> Produtos { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string? Observacao { get; set; }
 }
