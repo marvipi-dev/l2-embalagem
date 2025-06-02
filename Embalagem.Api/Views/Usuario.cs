969776756ddee59c1295f0d0929ee7b2e76642c1
Marcus Vinicius Pires <211013457+marvipi-dev@users.noreply.github.com>
2025-06-02 09:59:48 -0300

    Require email and password for authentication

--- Embalagem.Api/Views/Usuario.cs
+++ Embalagem.Api/Views/Usuario.cs
@@ -5,8 +5,8 @@ namespace Embalagem.Api.Views;
 public class Usuario
 {
     [MaxLength(50, ErrorMessage = "O e-mail deve conter no máximo 50 caracteres.")]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     [MaxLength(128, ErrorMessage = "A senha deve conter no máximo 128 caracteres")]
-    public string Senha { get; set; }
+    public required string Senha { get; set; }
 }
